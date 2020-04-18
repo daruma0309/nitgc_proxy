4b4fa840bcf7fe0c5376851d90e98edd3c37c878
HibikiTakahashi <40554462+daruma0309@users.noreply.github.com>
2020-04-18 20:41:59 +0900

    Update nitgc_proxy.sh

--- nitgc_proxy.sh
+++ nitgc_proxy.sh
@@ -4,7 +4,7 @@ echo "setup environment variable"
 sh -c "echo \"export http_proxy=http://proxy.ice.gunma-ct.ac.jp:8080/\" >> ~/.bashrc"
 
 echo "setup apt-get"
-sh -c "echo \"Acquire::https::proxy "https://proxy.ice.gunma-ct.ac.jp:8080/";\" >> /etc/apt/apt.conf"
+sh -c "echo \"Acquire::http::proxy "http://proxy.ice.gunma-ct.ac.jp:8080/";\" >> /etc/apt/apt.conf"
 
 echo "setup wgetrc"
 sh -c "echo \"http_proxy = http://proxy.ice.gunma-ct.ac.jp:8080/\" >> /etc/wgetrc"
